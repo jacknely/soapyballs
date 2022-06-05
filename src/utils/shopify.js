657ec744a75f4a6d1ebedcb402b89bb8dceb3e70
Jack Nelson <jackn926@live.co.uk>
2022-06-05 22:31:32 +0100

    Update shopify.js

--- src/utils/shopify.js
+++ src/utils/shopify.js
@@ -1,9 +1,11 @@
-import Client from "shopify-buy";
+<script src="https://sdks.shopifycdn.com/js-buy-sdk/v2/latest/index.umd.min.js"></script>
 
+<script>
 // Initializing a client to return content in the store's primary language
 const client = Client.buildClient({
-  domain: "https://soapyballs.myshopify.com",
+  domain: "soapyballs.myshopify.com",
   storefrontAccessToken: "63f5ec83fe6705f561abef517947b5f7",
 });
 
 export { client };
+</script>
