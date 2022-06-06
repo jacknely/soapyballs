3252513191c758b03f5e693e2e12273d22fc8365
Jack <jackn926@live.co.uk>
2022-06-06 19:45:48 +0100

    upped serverles

--- src/utils/shopify.js
+++ src/utils/shopify.js
@@ -1,9 +1,6 @@
-import Client from "shopify-buy";
+
 
 // Initializing a client to return content in the store's primary language
-const client = Client.buildClient({
-  domain: "soapyballs.myshopify.com",
-  storefrontAccessToken: "63f5ec83fe6705f561abef517947b5f7",
-});
+const client = {'test': 'test'}
 
 export { client };
